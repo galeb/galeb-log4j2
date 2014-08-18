4763a0c0b8e09cf7e51995af4de763ad41be0a31
Marcelo Monteiro <m.monteiro@corp.globo.com>
2014-08-18 10:42:38 -0300

    initial commit

---
+++ src/main/java/lbaas/vertx/log4j2/Log4j2LogDelegate.java
@@ -0,0 +1,78 @@
+/* 
+ * Copyright (c) 2014 The original author or authors.
+ * All rights reserved.
+ */
+package lbaas.vertx.log4j2;
+
+import org.vertx.java.core.logging.impl.LogDelegate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j2LogDelegate implements LogDelegate {
+
+    private final Logger logger;
+
+    Log4j2LogDelegate(final String name) {
+        logger = LogManager.getLogger(name);
+    }
+
+    public boolean isInfoEnabled() {
+        return logger.isInfoEnabled();
+    }
+
+    public boolean isDebugEnabled() {
+        return logger.isDebugEnabled();
+    }
+
+    public boolean isTraceEnabled() {
+        return logger.isTraceEnabled();
+    }
+
+    public void fatal(final Object message) {
+        logger.fatal(message);
+    }
+
+    public void fatal(final Object message, final Throwable t) {
+        logger.fatal(message, t);
+    }
+
+    public void error(final Object message) {
+        logger.error(message);
+    }
+
+    public void error(final Object message, final Throwable t) {
+        logger.error(message, t);
+    }
+
+    public void warn(final Object message) {
+        logger.warn(message);
+    }
+
+    public void warn(final Object message, final Throwable t) {
+        logger.warn(message, t);
+    }
+
+    public void info(final Object message) {
+        logger.info(message);
+    }
+
+    public void info(final Object message, final Throwable t) {
+        logger.info(message, t);
+    }
+
+    public void debug(final Object message) {
+        logger.debug(message);
+    }
+
+    public void debug(final Object message, final Throwable t) {
+        logger.debug(message, t);
+    }
+
+    public void trace(final Object message) {
+        logger.trace(message);
+    }
+
+    public void trace(final Object message, final Throwable t) {
+        logger.trace(message, t);
+    }
+}
---
+++ src/main/java/lbaas/vertx/log4j2/Log4j2LogDelegateFactory.java
@@ -0,0 +1,16 @@
+/* 
+ * Copyright (c) 2014 The original author or authors.
+ * All rights reserved.
+ */
+package lbaas.vertx.log4j2;
+
+import org.vertx.java.core.logging.impl.LogDelegate;
+import org.vertx.java.core.logging.impl.LogDelegateFactory;
+
+public class Log4j2LogDelegateFactory implements LogDelegateFactory {
+
+    public LogDelegate createDelegate(String name) {
+        return new Log4j2LogDelegate(name);
+    }
+
+}
